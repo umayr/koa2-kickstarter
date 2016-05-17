dbc36e99a06a346e86c8107926cfe6ed21fef4a3
Umayr Shahid <umayrr@hotmail.co.uk>
2016-05-17 18:46:11 +0500

    Integrate boom errors

--- route/user.js
+++ route/user.js
@@ -1,6 +1,8 @@
 'use strict';
 
 import Router from 'koa-router';
+import Errors from 'boom';
+
 import * as Ctrl from '../controller/user';
 
 const router = new Router({
@@ -12,5 +14,9 @@ router.post('/', Ctrl.post);
 router.put('/', Ctrl.put);
 
 export const Routes = router.routes();
-// TODO: add boom support for errors: https://github.com/hapijs/boom
-export const AllowedMethod = router.allowedMethods();
+
+export const AllowedMethod = router.allowedMethods({
+    throw: true,
+    notImplemented: () => new Errors.notImplemented(),
+    methodNotAllowed: () => new Errors.methodNotAllowed()
+});
