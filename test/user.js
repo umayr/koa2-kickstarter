4a670167dd33555726a6ea9b7971ed9f13fab06e
Umayr Shahid <umayrr@hotmail.co.uk>
2016-05-18 03:40:57 +0500

    Add unit tests for user service

---
+++ test/user.js
@@ -0,0 +1,17 @@
+import test from 'ava';
+import * as service from '../src/service/user';
+
+test('should return users', t => {
+  const users = service.getUsers();
+
+  t.is(users.length, 4);
+});
+
+test('should return valid names', t => {
+  const [ava, boyd, raylan, winona] = service.getUsers();
+
+  t.not(ava, undefined);
+  t.not(boyd, undefined);
+  t.not(raylan, undefined);
+  t.not(winona, undefined);
+});
